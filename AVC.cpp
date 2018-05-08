b75b8606ae8ef85befb4331d10028abf3b49eadc
mikekiwi99 <36977168+mikekiwi99@users.noreply.github.com>
2018-05-08 15:52:15 +1200

    put error finder into own method

--- AVC.cpp
+++ AVC.cpp
@@ -4,6 +4,7 @@
 
 int followLine();
 int followMaze();
+int findCurveError();
 
 int redCount = 0; //
 int wallClose = 200; //200 is test value change later. constant for what a close wall reads
@@ -25,6 +26,19 @@ int main(){
 
 int followLine(){
 	
+	int error = findCurveError();
+	
+	printf("%d", error);
+	
+	return 0;
+}
+
+int findCurveError(){
+	
+	//stores how far to the left or right the line is
+	int error = 0;
+	int numberWhites = 0;
+	
 	//take picture
 	take_picture();
 	//set row to scan
@@ -34,14 +48,14 @@ int followLine(){
     int min =255;
     
     //for all pixels on row, find the max and min white value
-   	for (int i = 0; i <320;i++){
+   	for(int i = 0; i <320;i++){
 		int pix = get_pixel(scan_row,i,3);
         //get max if larger
-        if ( pix > max){
+        if( pix > max){
 			max = pix;
 		}
 		//get min if smaller
-		if (pix < min){
+		if(pix < min){
 			min =pix;
 		}
     }
@@ -49,26 +63,32 @@ int followLine(){
     int threshold = (max+min)/2;
     
     printf(" min=%d max=%d threshold=%d\n", min, max,threshold); //testing for finding min/max/threshold
+       
+    int white = 0;//white value (100 or 200)
     
-    //create array holding boolean value stating whether a pixel is white or not, for all pixels
-    int white[320];
-    //go through all pixels. if below threshold its not a white pixel. if above then it is.
-    for (int i = 0; i <320;i++){
-		white[i]= 0 ;
+    //go through all pixels. if below threshold its not a white pixel. if above then it is white
+    for(int i = 0; i <320;i++){
 		int pix = get_pixel(scan_row,i,3);
-		if ( pix > threshold){
-			white[i] = 1;
+		if( pix > threshold){ //pixel is 'white'
+			//white value of pixel is 100 (not white) and don't add to number of white pixels
+			white = 200;
+			numberWhites += 1;
+			
+		}else{ //pixel is 'black'
+			//white value of pixel set to 200 and add to number of white pixels
+			white = 100;
+			
 		}
-    }
-    
-    for (int i = 0; i <320;i++){
-		printf("%d ",white[i]); //print if pixel was white or not, for each pixel.
+		//add to error the weight and value of pixel (distance from center and whiteness)
+		error = error + (i - 160)*white;
+		
     }
     printf("\n");
 	
-	redCount ++; //test to move on
+	//normalising error for number of pixels
+	error = error / numberWhites;
 	
-	return 0;
+	return error;
 }
 
 int followMaze(){
